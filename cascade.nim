c2a8f5c453d9be609e2dda25ac634e876f82c60d
Bo Lingen <lingenbw@gmail.com>
2019-01-28 23:04:55 +0000

    feat: support many more constructs
    Properly support various proc call styles & artities and arbitrary depths
    of dot expressions. Add `if` and `when` conditional support. Greatly
    improve test coverage.

--- cascade.nim
+++ cascade.nim
@@ -1,29 +1,83 @@
 import macros
 
-macro cascade* (obj: typed, body: untyped): auto =
+# rewrite a tree of dot expressions to make the
+# given `id` the topmost object in the chain
+proc rewriteDotExpr (id, dotExpr: NimNode): NimNode =
+  var lhs = dotExpr[0]
+  var rhs = dotExpr[1]
+
+  if lhs.kind != nnkDotExpr:
+    return newDotExpr(newDotExpr(id, lhs), rhs)
+
+  result = newDotExpr(id, rhs)
+  var chain: seq[NimNode]
+
+  while true:
+    if lhs.kind == nnkDotExpr:
+      rhs = lhs[1]
+      lhs = lhs[0]
+      chain.add rhs
+    else:
+      chain.add lhs
+      break
+
+  for i in countdown(chain.len - 1, 0):
+    result[0] = newDotExpr(result[0], chain[i])
+
+macro cascade* (obj: typed, body: untyped): untyped =
   let statements =
     if body.kind == nnkDo: body[6]
     else: body
   statements.expectKind(nnkStmtList)
 
-  let id = genSym(kind = nskVar)
-  let assignment = newVarStmt(id, obj)
-  result = newStmtList(assignment)
-  for statement in statements:
-    case statement.kind:
-    of nnkAsgn:
-      result.add(
-        newAssignment(
-          newDotExpr(id, statement[0]),
-          statement[1]
-        )
-      )
-    of nnkCall:
-      result.add(
-        newCall(
-          newDotExpr(id, statement[0])
-        )
-      )
-    else: discard
+  proc transform (id, statementList: NimNode, init = newStmtList()): NimNode =
+    result = init
+    for statement in statementList:
+      case statement.kind
+      of nnkIfStmt, nnkWhenStmt:
+        # recurse into each branch of `if` or `when`
 
-  result.add(id)
+        for branch in statement:
+          case branch.kind
+          of nnkElifBranch:
+            branch[1] = transform(id, branch[1])
+          of nnkElse:
+            branch[0] = transform(id, branch[0])
+          else:
+            discard
+
+        result.add statement
+      of nnkAsgn:
+        var lhs = statement[0]
+        if lhs.kind == nnkDotExpr:
+          lhs = rewriteDotExpr(id, lhs)
+        else:
+          lhs = newDotExpr(id, lhs)
+
+        result.add newAssignment(lhs, statement[1])
+      of nnkCall, nnkCommand:
+        var call: NimNode
+        if statement[0].kind == nnkDotExpr:
+          call = newCall(rewriteDotExpr(id, statement[0]))
+        else:
+          call = newCall(statement[0], id)
+
+        for i in 1 ..< statement.len:
+          call.add statement[i]
+
+        result.add call
+      else:
+        result.add statement
+
+  # create a `var` declaration with a unique generated identifier name
+  let id = genSym(kind = nskVar)
+  # assign the initial object to this new `var`
+  let assignment = newVarStmt(id, obj)
+
+  # kick off transformation on a new statement list
+  # each `statement` in `statements` will be added to this
+  # new statement list after being bound to the initial object
+  result = transform(id, statements, newStmtList(assignment))
+
+  # return the initial object
+  result.add id
--- tests/tests.nim
+++ tests/tests.nim
@@ -2,16 +2,152 @@ import unittest
 
 import cascade
 
-type Button = object
-  text: string
-  width: int
-  color: string
+type
+  Qux = object
+    ns: seq[int]
+
+  Baz = object
+    qux: Qux
+
+  Bar = object
+    n: int
+    baz: Baz
+
+  Foo = object
+    str: string
+    num: int
+    list: seq[string]
+    bar: Bar
+
+  Button = object
+    text: string
+    width: int
+    color: string
 
 method btnEcho (btn: Button) {.base.} =
   discard "some method"
 
+proc setStr (foo: var Foo, val: string, suffix: string) =
+  foo.str = "hello " & val & suffix
+
+template blk (foo: Foo, body: untyped) =
+  body
+
 suite "cascade":
-  test "equivalent to original `var` usage":
+  test "basic field assignment":
+    let foo = cascade Foo():
+      str = "hello"
+      num = 10
+
+    check foo == Foo(str: "hello", num: 10)
+
+  test "calling procs on fields":
+    let foo = cascade Foo():
+      list.add "one"
+      list.add "two"
+
+    check foo == Foo(list: @["one", "two"])
+
+  test "calling procs on fields using named parameters":
+    let foo = cascade Foo():
+      setStr(suffix = "!", val = "world")
+
+    check foo == Foo(str: "hello world!")
+
+  test "nested field assignment":
+    let foo = cascade Foo():
+      bar.n = 100
+
+    check foo == Foo(bar: Bar(n: 100))
+
+  test "nested cascades":
+    let foo = cascade Foo():
+      bar = cascade Bar():
+        n = 6
+        baz = cascade Baz():
+          qux = cascade Qux():
+            ns = @[1, 2, 3, 4]
+
+    let expected = Foo(
+      bar: Bar(
+        n: 6,
+        baz: Baz(
+          qux: Qux(
+            ns: @[1, 2, 3, 4]
+          )
+        )
+      )
+    )
+
+    check foo == expected
+
+  test "deeply nested proc calls":
+    let foo = cascade Foo():
+      bar.baz.qux.ns.add 1
+
+    let expected = Foo(
+      bar: Bar(
+        baz: Baz(
+          qux: Qux(
+            ns: @[1]
+          )
+        )
+      )
+    )
+
+    check foo == expected
+
+  test "bodies passed to calls are not modified":
+    var condition = false
+    let foo = cascade Foo():
+      blk:
+        condition = true
+
+    check condition
+
+  test "handles `if` conditionals":
+    let foo = cascade Foo():
+      if true:
+        str = "yes"
+      else:
+        str = "no"
+
+    check foo == Foo(str: "yes")
+
+  test "recursively handles `if` conditionals":
+    let foo = cascade Foo():
+      if true:
+        if false:
+          str = "yes"
+        else:
+          str = "yes, but no"
+      else:
+        str = "no"
+
+    check foo == Foo(str: "yes, but no")
+
+  test "handles `when` conditionals":
+    let foo = cascade Foo():
+      when true:
+        str = "yes"
+      else:
+        str = "no"
+
+    check foo == Foo(str: "yes")
+
+  test "recursively handles `when` conditionals":
+    let foo = cascade Foo():
+      when true:
+        when false:
+          str = "yes"
+        else:
+          str = "yes, but no"
+      else:
+        str = "no"
+
+    check foo == Foo(str: "yes, but no")
+
+  test "GUI button example":
     let btn = cascade Button():
       text = "ok"
       width = 30
