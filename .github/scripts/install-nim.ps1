314e30ceb90f073e46135666517de726e7ac0f34
citycide <lingenbw@gmail.com>
2019-10-05 00:53:32 -0500

    ci(github): add initial github action for tests

---
+++ .github/scripts/install-nim.ps1
@@ -0,0 +1,43 @@
+param(
+  [parameter(mandatory)]
+  [string] $nimVersion
+)
+
+set-strictMode -version 3
+$ErrorActionPreference = 'Stop'
+
+if (-not $isWindows) {
+  $initPath = join-path $home "init.sh"
+
+  $request = @{
+    uri     = "https://nim-lang.org/choosenim/init.sh"
+    outFile = $initPath
+  }
+
+  invoke-webrequest @request
+
+  sh $initPath
+  choosenim $nimVersion
+
+  remove-item $initPath | out-null
+} else {
+  # TODO: use choosenim on Windows once x64 is supported
+  # https://github.com/dom96/choosenim/issues/128
+
+  $packageName = "nim-1.0.0"
+  $zipPath = join-path (join-path $home ".gradle") "nim.zip"
+
+  $request = @{
+    uri     = "https://nim-lang.org/download/${packageName}_x64.zip"
+    outFile = $zipPath
+  }
+
+  invoke-webrequest @request
+
+  $extractDir = split-path $zipPath -parent
+  expand-archive $zipPath -destinationPath $extractDir
+
+  join-path $extractDir $packageName | set-location
+
+  & .\finish
+}
